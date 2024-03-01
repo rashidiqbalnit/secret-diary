dbedcdaadf38a726d55555dc7df567351448a068
rashidiqbalnit <rashidiqbalnit@gmail.com>
2024-03-02 01:28:40 +0530

    useLocation Hook & form section added

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,22 +2,9 @@ import { useState } from "react";
 import NoteContext  from "./noteContext";
 
 const NoteState=(props)=>{
-    const s1 = {
-        "name": "Rashid",
-        "class": "12th"
-    }
-    const [state, setState] = useState(s1);
-    const update = () =>{
-       setTimeout(()=>{
-         setState({
-            "name": "Harry",
-            "class": "10b"
-         })
-       },1000)
-    }
-
+    
     return(
-        <NoteContext.Provider value={{state, update}}>
+        <NoteContext.Provider value={{}}>
             {props.children}
         </NoteContext.Provider>
     )
